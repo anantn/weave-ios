4302185397387d0d803b70b13d75b9c0d9d6fb42
Anant Narayanan <anant@kix.in>
2009-06-29 15:11:08 -0700

    Add MPL license

--- Classes/TabViewController.m
+++ Classes/TabViewController.m
@@ -1,10 +1,26 @@
-//
-//  TabViewController.m
-//  Weave
-//
-//  Created by Anant Narayanan on 6/24/09.
-//  Copyright 2009 Mozilla Corporation. All rights reserved.
-//
+/***** BEGIN LICENSE BLOCK *****
+ Version: MPL 1.1
+ 
+ The contents of this file are subject to the Mozilla Public License Version 
+ 1.1 (the "License"); you may not use this file except in compliance with 
+ the License. You may obtain a copy of the License at 
+ http://www.mozilla.org/MPL/
+ 
+ Software distributed under the License is distributed on an "AS IS" basis,
+ WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ for the specific language governing rights and limitations under the
+ License.
+ 
+ The Original Code is weave-iphone.
+ 
+ The Initial Developer of the Original Code is Mozilla Labs.
+ Portions created by the Initial Developer are Copyright (C) 2009
+ the Initial Developer. All Rights Reserved.
+ 
+ Contributor(s):
+	Anant Narayanan <anant@kix.in>
+ 
+ ***** END LICENSE BLOCK *****/
 
 #import "TabViewController.h"
 #import "WeaveAppDelegate.h"
@@ -14,23 +30,6 @@
 
 @synthesize overlay, pgText, pgStatus, pgBar, tView;
 
-/*
- // The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-    if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
-        // Custom initialization
-    }
-    return self;
-}
-*/
-
-/*
-// Implement loadView to create a view hierarchy programmatically, without using a nib.
-- (void)loadView {
-}
-*/
-
-// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
     [super viewDidLoad];
 	[self setDelegate:self];
@@ -40,14 +39,6 @@
 	accel.delegate = self;
 	accel.updateInterval = 1.0f/10.0f;
 }
-
-/*
-// Override to allow orientations other than the default portrait orientation.
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-    // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
-}
-*/
 
 - (void)accelerometer:(UIAccelerometer *)accelerometer didAccelerate:(UIAcceleration *)acceleration {
 	if (fabsf(acceleration.x) > 1.6 ||
@@ -69,10 +60,7 @@
 }
 
 - (void)didReceiveMemoryWarning {
-	// Releases the view if it doesn't have a superview.
     [super didReceiveMemoryWarning];
-	
-	// Release any cached data, images, etc that aren't in use.
 }
 
 - (void)viewDidUnload {
--- Classes/WebViewController.m
+++ Classes/WebViewController.m
@@ -1,10 +1,26 @@
-//
-//  WebViewController.m
-//  Weave
-//
-//  Created by Anant Narayanan on 6/15/09.
-//  Copyright 2009 Mozilla Corporation. All rights reserved.
-//
+/***** BEGIN LICENSE BLOCK *****
+ Version: MPL 1.1
+ 
+ The contents of this file are subject to the Mozilla Public License Version 
+ 1.1 (the "License"); you may not use this file except in compliance with 
+ the License. You may obtain a copy of the License at 
+ http://www.mozilla.org/MPL/
+ 
+ Software distributed under the License is distributed on an "AS IS" basis,
+ WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ for the specific language governing rights and limitations under the
+ License.
+ 
+ The Original Code is weave-iphone.
+ 
+ The Initial Developer of the Original Code is Mozilla Labs.
+ Portions created by the Initial Developer are Copyright (C) 2009
+ the Initial Developer. All Rights Reserved.
+ 
+ Contributor(s):
+	Anant Narayanan <anant@kix.in>
+ 
+ ***** END LICENSE BLOCK *****/
 
 #import "WebViewController.h"
 #import "WeaveAppDelegate.h"
